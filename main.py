6ef897ea016f80c709b5519d69719a08b52e0fdf
democodingal <mayurirastogi0@gmail.com>
2024-10-22 22:25:13 +0530

    done

---
+++ main.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("welcome to the world of programming")
+
+#print a number
+print(2)
+
+#next line after print
+print("hello python \n")
+
+#pass multiple arguements for printing
+print("hello",5)
+
+#end arguement of print statement
+print("welcome to ", end= "*")
